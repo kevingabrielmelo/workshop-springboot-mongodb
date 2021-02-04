ea6932f2c9f0b328a9a3338b647fd5f65ba6f52e
Kevin Gabriel Alves de Melo <kevinalvesmelo@gmail.com>
2021-02-04 17:56:15 -0300

    Obtendo um usuario por id

--- src/main/java/com/kevinmelo/workshopmongo/services/UserService.java
+++ src/main/java/com/kevinmelo/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.kevinmelo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kevinmelo.workshopmongo.domain.User;
 import com.kevinmelo.workshopmongo.repository.UserRepository;
+import com.kevinmelo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
